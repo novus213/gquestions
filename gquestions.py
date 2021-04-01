a2cc5359591f85cfd66c69d828d6bf23fcf9a74f
Etienne M <44229417+novus213@users.noreply.github.com>
2021-04-01 15:17:17 +0200

    update selenium to heroku

--- gquestions.py
+++ gquestions.py
@@ -58,15 +58,12 @@ def prettyOutputName(filetype='html'):
 
 
 def initBrowser(headless=False):
-    if "Windows" in platform.system():
-        chrome_path = "driver/chromedriver.exe"
-    else:
-        chrome_path = "driver/chromedriver"
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-features=NetworkService")
-    if headless:
-        chrome_options.add_argument('headless')
-    return webdriver.Chrome(options=chrome_options,executable_path=chrome_path)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    return webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 """
 Search on Google and returns the list of PAA questions in SERP.
 """
